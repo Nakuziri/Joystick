9c47f8cd4cd7998b8a9ea6be5a9924851e74ea2a
Nakuziri <xcampos@brooklynsteamcenter.org>
2024-02-07 10:49:58 -0500

    working on fixing login

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import pymysql
 import pymysql.cursors
+import flask_login
 
 conn = pymysql.connect(
    database="xcampos_joystick",
@@ -12,6 +13,34 @@ conn = pymysql.connect(
 
 app = Flask(__name__)
 
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
+class user:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+
+    def __init__ (self,id,username):
+        self.username = username
+        self.id = id
+
+    def get_id(self):
+        return str(self.id)
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM `users` WHERE `id` = " + user_id)
+        result = cursor.fetchone()
+        cursor.close()
+        cursor.commit()
+
+        if result is None:
+            return None
+        
+        return user(result["id"],result['username'])
 @app.route('/land')
 def index():
     user_name = 'goodjob'
@@ -33,3 +62,30 @@ def register():
         conn.commit()
     
     return render_template("register.html")
+
+@app.route('/sign_in', methods=['GET','POST'])
+def sign_in():
+    if request.method == 'GET':
+        usernameLog = request.form['usernameLog']
+        passwordLog = request.form['passwordLog']
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM `users` WHERE `Username` = '{usernameLog}' ")
+
+        result = cursor.fetchone()
+
+        cursor.close()
+        cursor.commit()
+
+    if request.form['passwordlog'] == result['password']:
+        user = load_user(result['id'])
+        flask_login.login_user(user)
+
+
+
+
+        return redirect('/feed')
+
+    return  redirect ('/feed')
+
+
+app.secret_key = 'bombaclaaattttt'
